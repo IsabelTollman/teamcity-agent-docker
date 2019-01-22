42d64c7162aa9cb94f2cbcf4b1f304cca736e488
IsabelTollman <41477690+IsabelTollman@users.noreply.github.com>
2019-01-22 16:31:34 +0200

    Change line 24

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,6 +21,6 @@ if [ ! -d "$AGENT_DIR/bin" ]; then
 fi
 
 echo "Starting buildagent..."
-chown -R team-city:team-city /opt/buildAgent
+RUN chown -R teamcity:teamcity /opt/buildAgent
 
 wrapdocker gosu teamcity /opt/buildAgent/bin/agent.sh run
