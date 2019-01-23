93db552e0835b6fe789cf9cdb214456c2c4502cd
IsabelTollman <41477690+IsabelTollman@users.noreply.github.com>
2019-01-23 10:13:37 +0200

    Reintroduce original line 30

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -27,4 +27,4 @@ chmod +x ./docker-entrypoint.sh
 
 # ls -la /usr/local/bin/docker-entrypoint.sh 
 # ls -la wrapdocker
-# root gosu wrapper /opt/buildagent/bin/agent.sh run # agent.sh # root
+wrapdocker gosu root /opt/buildagent/bin/agent.sh run # agent.sh # root
