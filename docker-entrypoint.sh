cad163ab7273188cf6d01f87a5f097a882f33891
IsabelTollman <41477690+IsabelTollman@users.noreply.github.com>
2019-01-23 08:15:41 +0200

    Run output line 27

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,4 +24,5 @@ echo "Starting buildagent..."
 ls -lart /opt/buildagent
 chown -R root:root /opt/buildagent
 
+ls -la /docker-entrypoint.sh 
 wrapdocker gosu root /opt/buildagent/bin/docker-entrypoint.sh run # agent.sh
